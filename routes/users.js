f036c4df63f2d864eb6aa171b792de056026cce9
hieptran <hiep.tran2@asiantech.vn>
2018-06-08 15:24:58 +0700

    joi validation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var userController = require('../controller/userController');
+const validate = require('express-validation');
+var userValidator = require('../lib/validation/UserValidations');
 
+router.get('/', userController.getList);
+
+router.get('/:id', userController.getUser);
+
+router.get('/:name/:phone', userController.findtUserByNameAndPhone);
+
+router.post('/add', validate(userValidator.register), userController.addUser);
+router.put('/update/:id', userController.updateUser);
+
+router.delete('/delete/:id', userController.deleteUserId);
+ 
 module.exports = router;
